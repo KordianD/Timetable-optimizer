4bc8d9da7e6ef571fa6ea054a19fb2b76ac76188
KordianD <kordiandrozd@gmail.com>
2017-11-19 10:12:49 +0100

    Small refactor

--- bees_algorithm.py
+++ bees_algorithm.py
@@ -2,19 +2,20 @@ import numpy as np
 import pandas as pd
 
 class Student:
-	def __init__(self, id, firstName='Jan', lastName='Kowalski'):
-		self.id = id
-		self.firstName = firstName
-		self.lastName = lastName
-		
+    def __init__(self, id, first_name='Jan', last_name='Kowalski'):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
+
 class Subject:
-	def __init__(self, name, numOfTerms=4):
-		self.name = name
-		self.numOfTerms = numOfTerms
-		
-numOfStudents = 50
-namesOfSubjects = ['Sieci komputerowe', 'Systemy pomiarowe', 'Teoria sterowania', 'Badania operacyjne',
-    				    'Technika mikroprocesorowa', 'Podstawy robotyki', 'Teoria optymalizacji']
-				   
-students = [Student(id) for id in range(0, numOfStudents)]
-subjects = [Subject(nameOfSubject) for nameOfSubject in namesOfSubjects]
+    def __init__(self, name, num_of_terms=4):
+        self.name = name
+        self.num_of_terms = num_of_terms
+
+NUM_OF_STUDENTS = 50
+NAMES_OF_SUBJECTS = ['Sieci komputerowe', 'Systemy pomiarowe', 'Teoria sterowania', 'Badania operacyjne',
+                     'Technika mikroprocesorowa', 'Podstawy robotyki', 'Teoria optymalizacji']
+
+students = [Student(id) for id in range(0, NUM_OF_STUDENTS)]
+subjects = [Subject(nameOfSubject) for nameOfSubject in NAMES_OF_SUBJECTS]
