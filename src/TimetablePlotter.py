ab74a1cff9006f6083af9f975466dab60c0e3513
KordianD <kordiandrozd@gmail.com>
2017-12-07 23:52:18 +0100

    First version of TimetablePlotter added

---
+++ src/TimetablePlotter.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+from configuration import *
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+colors = ['pink', 'lightgreen', 'lightblue', 'wheat', 'salmon']
+day_label = ['Timetable']
+
+fig_width = 10
+fig_height = 6
+
+def convert_hour(hour):
+    return STARTING_HOUR + hour * DIFFERENCE_BETWEEN_STARTING_CLASSES_IN_HOURS
+
+def plot_timetable(student):
+    fig = plt.figure(figsize=(fig_width, fig_height))
+
+    print(student.timetable)
+
+    for classwork in student.timetable:
+        classwork_name = classwork.classwork_name
+
+        day = classwork.day
+        start_hour = convert_hour(classwork.hour)
+        end_hour = start_hour + DURATION_OF_ONE_CLASSWORK_IN_HOURS
+
+        plt.fill_between([day, day + 0.96], [int(start_hour), int(start_hour)], [int(end_hour), int(end_hour)],
+                         color=colors[day], edgecolor='k', linewidth=0.5)
+
+
+        # plot beginning time
+        plt.text(day + 0.02, start_hour + 0.05, '{0}:{1:0>2}'.format(int(start_hour), start_hour - int(start_hour)),
+                 va='top', fontsize=7)
+
+        # plot event name
+        plt.text(day + 0.48, (start_hour + end_hour) * 0.5, classwork_name, ha='center', va='center', fontsize=11)
+
+
+    # Set Axis
+    ax=fig.add_subplot(111)
+    ax.yaxis.grid()
+    ax.set_xlim(0.5, len(days) + 0.5)
+    ax.set_ylim(15.1, 8.9)
+    ax.set_xticks(range(1, len(days) + 1))
+    ax.set_xticklabels(days)
+    ax.set_ylabel('Time')
+
+    # Set Second Axis
+    ax2=ax.twiny().twinx()
+    ax2.set_xlim(ax.get_xlim())
+    ax2.set_ylim(ax.get_ylim())
+    ax2.set_xticks(ax.get_xticks())
+    ax2.set_xticklabels(days)
+    ax2.set_ylabel('Time')
+
+
+    plt.title(day_label, y=1.07)
+    plt.savefig('{0}.png'.format(day_label), dpi=200)
+
+    plt.show()
+
